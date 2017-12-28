a8acfed36084b5b560bb4825660c4df736ab020a
Tech-A66 <tech-a66@Tech-A66s-Air.home>
2017-12-28 21:25:13 +0000

    rspec added

--- project_neo/config.ru
+++ project_neo/config.ru
@@ -4,5 +4,6 @@ require 'httparty'
 require 'json'
 
 require_relative './controllers/neo_controller.rb'
+# require_relative './lib/services/neo_data.rb'
 
 run NeoControl
--- project_neo/lib/services/neo_data.rb
+++ project_neo/lib/services/neo_data.rb
@@ -14,6 +14,9 @@ class CurrentNeoData
     @current_neo_data_id = JSON.parse(self.class.get("/#{id}?api_key=z9t2OsrUm9tT1D3ccMFVXzC69q6wN7K0CRXuJvQy").body)
   end
 
+  def neo_ex_id
+    get_neo_id(3542519)
+  end
 
   #by Date
   # base_uri 'https://api.nasa.gov/neo/rest/v1/feed?'
@@ -21,17 +24,13 @@ class CurrentNeoData
   # def get_neo(start_date, end_date)
   #   @current_neo_data = JSON.parse(self.class.get("start_date=#{start_date}&end_date=#{end_date}&api_key=z9t2OsrUm9tT1D3ccMFVXzC69q6wN7K0CRXuJvQy").body)
   # end
-
-  def neo_ex
-    get_neo('2015-09-07', '2015-09-08')
-  end
-
-  def neo_ex_id
-    get_neo_id(3542519)
-  end
+  #
+  # def neo_ex
+  #   get_neo('2015-09-07', '2015-09-08')
+  # end
 
 end
 
 x = CurrentNeoData.new
-p x.neo_ex_id
+# p x.neo_ex_id
 # p x.neo_ex
