1312e23932ac725727aac9aefc4c46538d17c5b9
Tech-A66 <tech-a66@Tech-A66s-Air.home>
2018-01-07 11:47:26 +0000

    cpybr test run when racked up

--- spec/project_neo_web_spec.rb
+++ spec/project_neo_web_spec.rb
@@ -8,6 +8,7 @@ describe 'users should be able to navigate around the website' do
       @neo_site = NeoDataTestClass.new
       @neo_site.nav_test.visit_home_page
       @neo_site.nav_test.click_browse_link
+      sleep 3
     end
   end
 end
